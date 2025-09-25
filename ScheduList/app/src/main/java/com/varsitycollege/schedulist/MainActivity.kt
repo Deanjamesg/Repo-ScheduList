3f6a1d50c511e8ccb6abd4719216f3c16fbcbed5
Dean James <deanjamesgreeff@gmail.com>
2025-09-25 18:09:20 +0200

    feat: Added navigation from dashboard
    Added navigation from the dashboard to:
    - Tasks
    - Simple List
    - Events
    - Settings
    
    Also added back navigation (navigate up) to go back to the dashboard from the other fragments.
    
    Implemented a top tool bar, so display the name of the fragment, and a back arrow to redirect the user back to the dashboard.
    
    Created a "More" button in the tool bar that displays a menu of all the app's features (no functionality currently) - To be implemented later.

--- ScheduList/app/src/main/java/com/varsitycollege/schedulist/MainActivity.kt
+++ ScheduList/app/src/main/java/com/varsitycollege/schedulist/MainActivity.kt
@@ -1,23 +1,57 @@
 package com.varsitycollege.schedulist
 
 import android.os.Bundle
+import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.varsitycollege.schedulist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        enableEdgeToEdge()
+        enableEdgeToEdge(SystemBarStyle.dark(1))
 
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        setContentView(view)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, 0, systemBars.right, 0)
             insets
         }
+
+        setSupportActionBar(binding.toolBar)
+
+        binding.toolBar.overflowIcon = AppCompatResources.getDrawable(this@MainActivity, R.drawable.ic_more_vert)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
