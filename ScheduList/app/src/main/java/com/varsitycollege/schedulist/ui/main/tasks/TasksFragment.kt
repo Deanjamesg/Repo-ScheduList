596948d2f4e3a5f02cc378451f82248ae743bbd2
Zack David Stangroom <11stangroomz@gmail.com>
2025-10-07 18:26:58 +0200

    removed redundant code

--- ScheduList/app/src/main/java/com/varsitycollege/schedulist/ui/main/tasks/TasksFragment.kt
+++ ScheduList/app/src/main/java/com/varsitycollege/schedulist/ui/main/tasks/TasksFragment.kt
@@ -15,7 +15,7 @@ import com.varsitycollege.schedulist.ui.adapter.MonthGridAdapter
 import com.varsitycollege.schedulist.ui.adapter.TasksAdapter
 import com.varsitycollege.schedulist.R
 import androidx.lifecycle.Observer
-//import com.varsitycollege.schedulist.ui.main.simplelist.TasksViewModel
+
 
 // This is our TasksFragment. It's in charge of the UI.
 // It sets up the views, listens for user input (like spinner clicks),
@@ -117,26 +117,3 @@ class TasksFragment : Fragment() {
     }
 }
 
-
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//        val spinnerTaskList = view.findViewById<android.widget.Spinner>(R.id.spinnerTaskList)
-//        val spinnerViewType = view.findViewById<android.widget.Spinner>(R.id.spinnerViewType)
-//
-//        val viewTypeItems = listOf("List View", "Grid View")
-//        val viewTypeAdapter = android.widget.ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, viewTypeItems)
-//        viewTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-//        spinnerViewType.adapter = viewTypeAdapter
-//
-//        // Adapter for task list, initially empty
-//        val taskListAdapter = android.widget.ArrayAdapter<String>(requireContext(), android.R.layout.simple_spinner_item, mutableListOf())
-//        taskListAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-//        spinnerTaskList.adapter = taskListAdapter
-//
-//        // Observe the ViewModel's task list names
-//        viewModel.taskListNames.observe(viewLifecycleOwner, Observer { newList ->
-//            taskListAdapter.clear()
-//            taskListAdapter.addAll(newList)
-//            taskListAdapter.notifyDataSetChanged()
-//        })
-//   }
